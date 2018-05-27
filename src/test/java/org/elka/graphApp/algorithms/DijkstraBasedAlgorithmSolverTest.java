83f14fa211841f848808fc7077d80ac3bff4c42f
Piotr Ganicz <piotr.ganicz@gmail.com>
2018-05-27 15:00:46 +0200

    Fix package for tests

--- src/test/java/org/elka/graphApp/algorithms/DijkstraBasedAlgorithmSolverTest.java
+++ src/test/java/org/elka/graphApp/algorithms/DijkstraBasedAlgorithmSolverTest.java
@@ -1,5 +1,6 @@
-package org.elka.graphApp;
+package org.elka.graphApp.algorithms;
 
+import org.elka.graphApp.MyWeightedEdge;
 import org.elka.graphApp.algorithms.DijkstraBasedAlgorithmSolver;
 import org.elka.graphApp.generators.MySimpleGraphGenerator;
 import org.jgrapht.Graph;
